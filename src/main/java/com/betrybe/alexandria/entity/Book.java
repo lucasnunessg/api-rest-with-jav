8c3a9f69d13151b013ae08987014ddaafeaa2bb9
lucasnunessg <lucasnunespacheco@gmail.com>
2024-09-13 10:05:24 -0300

    criando as entidades e iniciando repository

---
+++ src/main/java/com/betrybe/alexandria/entity/Book.java
@@ -0,0 +1,48 @@
+package com.betrybe.alexandria.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String genre;
+
+    public Book() {
+
+    }
+
+    public Book(Long id, String title) {
+        this.title = title;
+        this.genre = genre;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}
