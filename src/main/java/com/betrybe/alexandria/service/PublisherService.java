379d5030ebfef38d862fea7b3f836bc9de037286
lucasnunessg <lucasnunespacheco@gmail.com>
2024-09-13 10:48:52 -0300

    services and exceptions

---
+++ src/main/java/com/betrybe/alexandria/service/PublisherService.java
@@ -0,0 +1,46 @@
+package com.betrybe.alexandria.service;
+
+import com.betrybe.alexandria.entity.Publisher;
+import com.betrybe.alexandria.exception.PublisherNotFoundException;
+import com.betrybe.alexandria.repository.PublisherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PublisherService {
+
+    private final PublisherRepository publisherRepository;
+
+    @Autowired
+    public PublisherService(PublisherRepository publisherRepository) {
+        this.publisherRepository = publisherRepository;
+    }
+
+    public Publisher findById(Long id) throws PublisherNotFoundException{
+        return publisherRepository.findById((id))
+                .orElseThrow(PublisherNotFoundException::new);
+    }
+
+    public List<Publisher> findAll() {
+        return publisherRepository.findAll();
+    }
+
+    public Publisher update(Long id, Publisher publisher) throws PublisherNotFoundException {
+        Publisher publisherFromDb = findById(id);
+
+        publisherFromDb.setName(publisher.getName());
+        publisherFromDb.setAddress(publisher.getAddress());
+
+        return publisherRepository.save(publisherFromDb);
+    }
+
+    public Publisher deleteById(Long id) throws PublisherNotFoundException {
+        Publisher publisher = findById(id);
+
+        publisherRepository.deleteById(id);
+
+        return publisher;
+    }
+}
