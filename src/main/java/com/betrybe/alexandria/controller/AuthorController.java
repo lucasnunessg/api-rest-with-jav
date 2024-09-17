10f598edf562f37e7effe14370900afe5d87ab30
lucasnunessg <lucasnunespacheco@gmail.com>
2024-09-17 09:43:11 -0300

    criando os controllers

---
+++ src/main/java/com/betrybe/alexandria/controller/AuthorController.java
@@ -0,0 +1,19 @@
+package com.betrybe.alexandria.controller;
+
+import com.betrybe.alexandria.service.AuthorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthorController {
+
+    private final AuthorService authorService;
+
+    @Autowired
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+
+}
