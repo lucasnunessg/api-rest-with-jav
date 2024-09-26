3e52c7f4724f50e45e4a0a56b8b21797bd650506
lucasnunessg <lucasnunespacheco@gmail.com>
2024-09-26 18:32:00 -0300

    manipulando mais horas

--- src/main/java/com/betrybe/alexandria/Clock.java
+++ src/main/java/com/betrybe/alexandria/Clock.java
@@ -6,20 +6,18 @@ import java.time.LocalDateTime;
 
 public class Clock {
     public static void main(String[] args) {
-        //do local do sistema
-        LocalDateTime now = LocalDateTime.now();
-        LocalDateTime currentDateNow = LocalDateTime.now();
-        System.out.println("Horario abaixo é o atual");
-        System.out.println(currentDateNow);
-        System.out.println("Diferenciar");
-        System.out.println(now);
-     //do fuso proximo
-       java.time.Clock relogio = java.time.Clock.systemDefaultZone();
-       System.out.println(relogio.instant());
-        LocalDateTime diaDoSofrimento = LocalDateTime.parse("2014-07-08T17:00:00");
-        System.out.println(diaDoSofrimento);
+    LocalDateTime currentDateTime = LocalDateTime.of(2024, 8, 19, 23,10,40,0);
+        LocalDateTime currentDateTime2 = LocalDateTime.of(2023, 8, 19, 23,10,40,0);
+
+    boolean isBefore = currentDateTime2.isBefore(currentDateTime);
+    boolean isBefore2 = currentDateTime.isBefore(currentDateTime2);
+    boolean isAfter = currentDateTime.isAfter(currentDateTime2);
+
+        System.out.println("aqui é pra dar true:");
+        System.out.println(isBefore);
+        System.out.println("aqui é pra dar false");
+        System.out.println(isBefore2);
+        System.out.println("aqui é pra dar true");
+        System.out.println(isAfter);
     }
-
-
-
 }
--- src/test/java/com/betrybe/alexandria/BookIntegrationTest.java
+++ src/test/java/com/betrybe/alexandria/BookIntegrationTest.java
@@ -8,8 +8,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -23,6 +28,18 @@ public class BookIntegrationTest {
 
     @Autowired
     MockMvc mockMvc;
+
+    @Container
+    public static MySQLContainer MYSQL_CONTAINER = new MySQLContainer("mysql:8.0.32")
+            .withDatabaseName("ecommerce");
+
+    @DynamicPropertySource
+    public static void overrideProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", MYSQL_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", MYSQL_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", MYSQL_CONTAINER::getPassword);
+    }
+
 
     @Test
     public void testBookCreation() throws Exception {
